e22477ade82d1bb9620910274b95c14be0ee37f5
HypnoticOcelot <57046530+RealHypnoticOcelot@users.noreply.github.com>
2022-08-06 20:56:00 -0400

    Add files via upload

--- tikgen.py
+++ tikgen.py
@@ -52,22 +52,6 @@ if folder == 0:
 if folder == 1:
     removeaudio = input(f"Remove audio from files in {bgfolder}? y/n: ")
 
-# if os.path.exists(f"{bgnomp4}cropped.mp4"): #if the no sound version already exists
-#     print(f"\n\"{bgnomp4}cropped.mp4\" already exists, skipping video cropping removal")
-#     pass
-# else:
-#     if bgnosnd.size[0] / bgnosnd.size[1] != 9/16 :
-#         not916 = input("\nYour video file does not match the aspect ratio! Crop it?(Only use this if your video is 16:9) y/n: ")
-#         if not916 == "n" or not916 == "N":
-#             pass
-#         else:
-#             if bgnosnd.size != [1920, 1080]:
-#                 bgnosnd = bgnosnd.resize( (1920, 1080) )
-#                 bgcrop = vfx.crop(bgnosnd,  x_center=960 , y_center=540, width=1080, height=1920)
-#                 bgcrop.write_videofile(f"{bgnomp4}cropped.mp4")
-
-# the above code does NOT work.
-
 volx = input("Would you like to increase clip sound? y/n: ")
 if volx == "Y" or "y":
     per = int(input("Percent? 1 is 100% volume, 2 is 200% volume, etc.: "))
@@ -75,67 +59,58 @@ else:
     per = 1
 
 loopable = input("Would you like to make the main video loopable? y/n: ")
-if loopable == "Y" or loopable == "y":
+if loopable.lower() == "y":
     loopamt = input("How many seconds of loop time? Recommended 0.5: ")
     try:
-        float(loopamt)
-    except ValueError:
+        float(loopamt) # test if it works as a float
+    except ValueError: # value error is the exception you'll get if it can't be a float
         print("Invalid input! Disabling loop...")
         loopable = "n"
-    else:
-        if float(loopamt) <= 0:
+    else: # else runs only if try doesn't have an exception
+        if float(loopamt) <= 0:  # if it's negative
             print("Invalid input! Disabling loop...")
             loopable = "n"
-else:
+else: # if you don't put in y
     print("Not looping video!")
     loopable = "n"
 
 
-addsubtext = input("Add subtext to the videos? y/n: ")
+addsubtext = input("Add subtext to the videos? y/n: ") # subtext is just another loop in the ending code
 if addsubtext.lower() == "y":
     subtextstr = input("What will the subtext be?: ")
 else:
-    addsubtext = "n"
+    addsubtext = "n" #any input besides y will make it become n
 
-print(f"\nProcessing {amtfiles} files!\n")
-for x in newfiles:
-    x = x + ".mp4"
-    xstr = x
-    xstr2 = xstr.replace(".mp4", "")
-    x = VideoFileClip(x)
-    x = x.resize(width=720)
-    durationsecs = x.duration
-    if folder == 1:
-        bgvid = bgfolder + "/" + str(random.choice(os.listdir(bgfolder)))
-        bgnosnd = bgv.without_audio()
-        bgnomp4 = bgvid.replace(".mp4", "")
-        if removeaudio == "y" or removeaudio == "Y":
+print(f"\nProcessing {amtfiles} files!\n") # cosmetic
+for x in newfiles: # loops for every file that follows the 1.mp4, 2.mp4 structure
+    x = x + ".mp4" # add back the file extension
+    xstr = x # i dunno clones x for some reason
+    xstr2 = xstr.replace(".mp4", "") # has a string version that has no .mp4
+    x = VideoFileClip(x) # convert to videofileclip
+    x = x.resize(width=720) # height will be computed
+    durationsecs = x.duration # duration of the file in seconds
+    if folder == 1: # if folder mode is enabled
+        bgvid = bgfolder + "/" + str(random.choice(os.listdir(bgfolder))) # random file from the folder you chose,
+        print(f"Using {bgvid} for final{xstr}!")
+        bgnosnd = bgv.without_audio() # defines the nosound version
+        bgnomp4 = bgvid.replace(".mp4", "") # remove the extension
+        if removeaudio == "y" or removeaudio == "Y": # if removing audio is enabled
             bgnosnd.write_videofile(f"{bgnomp4}nosound.mp4")
             bgnosnd = VideoFileClip(f"{bgnomp4}nosound.mp4")
         else:
-            bgnosnd = VideoFileClip(f"{bgnomp4}.mp4")
-        if bgnosnd.size != [720, 1080]:
+            bgnosnd = VideoFileClip(f"{bgnomp4}.mp4") # i don't even know if this is necessary but why not
+        if bgnosnd.size != [720, 1080]: # make sure it's 720x1080, or 720p
             bgnosnd = bgnosnd.resize( (720, 1080))
     fullclip = CompositeVideoClip([bgnosnd.set_position((0,0)).set_end(durationsecs), x.set_position((0,20))], size=(720, 1080))
     textclip = TextClip(f"Part {xstr2}", font="fjalla-one.ttf",fontsize=125, color="white", stroke_color="black", stroke_width=5)
     textclip = textclip.set_position('center').set_duration(durationsecs) 
-    fullclip = CompositeVideoClip([fullclip, textclip])
-    fullclip = fullclip.volumex(per)
-    if loopable != "n":
+    # ^^^ all just stuff about placing the clips properly and adding text
+    fullclip = CompositeVideoClip([fullclip, textclip]) #combine the video with the text we just define
+    fullclip = fullclip.volumex(per) # if you enabled multiplying volume, this would add more sound to the clip
+    if loopable != "n": # if looping the clip is enabled, add that in
         fullclip = vfx.make_loopable(fullclip, float(loopamt))
-    if addsubtext != "n":
+    if addsubtext != "n": # if subtext is enabled, add that in
         subtext = TextClip(subtextstr, font="fjalla-one.ttf",fontsize=75, color="white", stroke_color="black", stroke_width=3)
-        subtext = subtext.set_position(("center", 600)).set_duration(durationsecs) 
+        subtext = subtext.set_position(("center", 650)).set_duration(durationsecs) 
         fullclip = CompositeVideoClip([fullclip, subtext])
-    fullclip.write_videofile(f"final{xstr}", codec="libx264", audio_codec="aac")
-
-# if not916 != "n" or not916 != "N":
-#     removecrop = input(f"Would you like to remove {bgnomp4}cropped.mp4? y/n: ")
-#     if removecrop == "Y" or removecrop == "y":
-#         if os.path.exists(f"{bgnomp4}cropped.mp4"):
-#             os.remove(f"{bgnomp4}cropped.mp4")
-if removeaudio == "y" or removeaudio == "Y":
-    removesound = input(f"Would you like to remove {bgnomp4}nosound.mp4? y/n: ")
-    if removesound == "Y" or removesound == "y":
-        if os.path.exists(f"{bgnomp4}nosound.mp4"):
-            os.remove(f"{bgnomp4}nosound.mp4")
+    fullclip.write_videofile(f"final{xstr}", codec="libx264", audio_codec="aac") # write the file
